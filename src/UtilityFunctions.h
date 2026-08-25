565f5d3d05d11264d12e7e8fe4f190f07cf16acb
synaapse_2 <bhanu.mittal@gmail.com>
2026-08-25 18:26:03 -0500

    Add STRINGIFY macro definitions to UtilityFunctions.h

--- src/UtilityFunctions.h
+++ src/UtilityFunctions.h
@@ -103,6 +103,11 @@
 #define NVRAM_PERFS_WEB_STATUS_LOG_BUFFER_SIZE_PROP "weblogSize"
 #endif
 
+#ifndef STRINGIFY(x)
+#define STRINGIFY_IMPL(x) #x
+#define STRINGIFY(x) STRINGIFY_IMPL(x)
+#endif
+
 namespace UtilityFunctions
 {
 
